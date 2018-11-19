e46eb19117c169ff3726d905ce8d34c478a400e7
AkashSinghChouhan <37949976+AkashSinghChouhan@users.noreply.github.com>
2018-11-20 04:01:35 +0530

    second commit

--- Win32Project1/main.cpp
+++ Win32Project1/main.cpp
@@ -3,6 +3,18 @@
 
 int main() {
 	
-	Windows window(point(w,0) , w , h , "Part2");
+	Windows window(point(w, 0), w, h, "Part3");
+	Line line1(point(0, h / 4), point(w, h / 4), 3, FL_WHITE);
+	Line line2(point(w / 2, 0), point(w / 2, h / 4), 3, FL_WHITE);
+	Circle circle1(point(w / 2, h / 2), 25, 1, FL_GREEN, FL_BLUE);
+	Text text1(point(200, 50), FL_HELVETICA, 25, FL_RED, "INPUT");
+	Text text2(point(w / 2 + 200, 50), FL_HELVETICA, 25, FL_RED, "OUTPUT");
+	window.attach(line1);
+	window.attach(line2);
+	window.attach(text1);
+	window.attach(text2);
+	window.attach(circle1);
+	
+
 	return Fl::run();
 }
