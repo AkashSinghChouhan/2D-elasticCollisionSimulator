939693ad0c70876a1c50164163639f4f15c9afcf
AkashSinghChouhan <37949976+AkashSinghChouhan@users.noreply.github.com>
2018-11-25 19:34:03 +0530

    thr first draft(CPU version)

--- Win32Project1/Shape.h
+++ Win32Project1/Shape.h
@@ -26,6 +26,12 @@ struct Circle : Shape {
 		add(p);
 		move(-radius, -radius);
 	}
+
+	void relocate(int xnew, int ynew) {
+		pts[0].x = xnew - radius;
+		pts[0].y = ynew - radius;
+	}
+
 	void draw() {
 		fl_line_style(FL_SOLID, line_width);
 		fl_color(fill_color);
--- Win32Project1/main.cpp
+++ Win32Project1/main.cpp
@@ -1,35 +1,32 @@
 #include"GUI.h"
+#include<time.h>
 
+Windows window(point(w, 0), w, h, "testing_phase");
+Line line1(point(0, h / 4), point(w, h / 4), 3, FL_WHITE);
+Line line2(point(w / 2, 0), point(w / 2, h / 4), 3, FL_WHITE);
+//Circle circle1(point(w / 2, h / 2), 25, 1, FL_GREEN, FL_BLUE);
+Text text1(point(200, 50), FL_HELVETICA, 25, FL_RED, "INPUT");
+Text text2(point(w / 2 + 200, 50), FL_HELVETICA, 25, FL_RED, "OUTPUT");
 
+Slider slider1(point(50, 80), 100, 30, "Particle speed.");
+In_box inbox1(point(50, 120), 100, 30, "Number of particles.");
 
-void callback1(Fl_Widget*, void*) {
-	ofstream out{ "collisions_vs_time.txt" };
-	out << "test" << endl;
-}
+Out_box outbox1(point(w / 2 + 50, 80), 100, 30, "Number of collisions.");
+Out_box outbox2(point(w / 2 + 50, 120), 100, 30, "Time elapsed (seconds).");
+
+#include"CallBack1.h"
+Button button1(point(200, 170), 100, 30, "RUN", callback1);
+Button button2(point(w / 2 + 200, 170), 100, 30, "PLOT", callback1);
 
 
 
 int main() {
-	
-	Windows window(point(w, 0), w, h, "testing_phase");
-	Line line1(point(0, h / 4), point(w, h / 4), 3, FL_WHITE);
-	Line line2(point(w / 2, 0), point(w / 2, h / 4), 3, FL_WHITE);
-	Circle circle1(point(w / 2, h / 2), 25, 1, FL_GREEN, FL_BLUE);
-	Text text1(point(200, 50), FL_HELVETICA, 25, FL_RED, "INPUT");
-	Text text2(point(w / 2 + 200, 50), FL_HELVETICA, 25, FL_RED, "OUTPUT");
-
-	Slider slider1(point(50, 80), 100, 30, "Particle speed.");
-	In_box inbox1(point(50, 120), 100, 30, "Number of particles.");
-	Button button1(point(200, 200), 100, 30, "RUN", callback1);
-	Out_box outbox1(point(w / 2 + 50, 80), 100, 30, "Number of collisions.");
-	Out_box outbox2(point(w / 2 + 50, 120), 100, 30, "Time elapsed (seconds).");
-	Button button2(point(w / 2 + 200, 200), 100, 30, "PLOT", callback1);
-
-	window.attach(line1);
+	srand(time(NULL));
+    window.attach(line1);
 	window.attach(line2);
 	window.attach(text1);
 	window.attach(text2);
-	window.attach(circle1);
+	//window.attach(circle1);
 
 	window.attach(slider1);
 	window.attach(inbox1);
